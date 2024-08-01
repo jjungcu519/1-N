ce53a299b944a231b4420844e5f41960a96b743e
jjungcu519 <jjungcu0519@gmail.com>
2024-08-01 14:42:15 +0900

    댓글 출력 방법 1,2 비교

--- articles/urls.py
+++ articles/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:id>/', views.detail, name='detail'),
     path('create/', views.create, name='create'),
+    # create 한 댓글을 생성? 관리하는 페이지
+    path('<int:article_id>/coments/create/', views.comment_create, name='comment_create'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Article
-from .forms import ArticleForm
+from .models import Article, Comment
+from .forms import ArticleForm,CommentForm
 
 # Create your views here.
 def index(request):
@@ -14,9 +14,14 @@ def index(request):
 
 def detail(request, id):
     article = Article.objects.get(id=id)
+    form = CommentForm()
+
+    comments = Comment.objects.filter(article_id=id)
 
     context = {
-        'article' : article
+        'article' : article,
+        'form' : form,
+        'comments' : comments,
     }
 
     return render(request, 'detail.html', context)
@@ -40,3 +45,25 @@ def create(request):
     }
     # 이름을 form.html로 한 이유 (두가지 기능 통합)
     return render(request, 'form.html', context)
+
+def comment_create(request, article_id):
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            #커밋하지말고 일단 기다려라 라는 뜻
+            comment = form.save(commit=False)
+
+            # #1. 사용자 입력 정보가 아닌 객체를 저장하여 article_id 받기(?)
+            # article = Article.objects.get(id=article_id)
+            # comment.article = article
+            # comment.save()
+            # return redirect('articles:detail', id=article_id)
+
+            #2. integer(숫자)를 저장하는 방법
+            comment.article_id = article_id
+            comment.save()
+            return redirect('articles:detail', id=article_id)
+
+
+    else:
+        return redirect('articles:index')
