2d0c50be2e9d04786d65fcc39bbdb1eab7f7e870
jjungcu519 <jjungcu0519@gmail.com>
2024-08-01 09:57:46 +0900

    modeling & migration

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    tittle = models.CharField(max_length=100)
+    content = models.TextField()
+
+# class Comment(models.Model):
+#     pass
