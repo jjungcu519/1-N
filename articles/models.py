7984793c741c2621772b6d59fcdbed6430e87207
jjungcu519 <jjungcu0519@gmail.com>
2024-08-01 11:59:04 +0900

    댓글을 불러들였다!

--- articles/models.py
+++ articles/models.py
@@ -4,7 +4,12 @@ from django.db import models
 class Article(models.Model):
     tittle = models.CharField(max_length=100)
     content = models.TextField()
+    # comment_set = 부모가 자식에게 접근할 수 있는 키 (자동 생성)
 
 class Comment(models.Model):
     content = models.TextField()
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    # article_id = 자식이 부모에게 접근할 수 있는 키 (자동 생성)
+
+    #comment = Comment.objects.get(id=id)
+
